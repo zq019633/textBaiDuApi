fc6e9242a70b33d8ac02c1caf3ea391ab39e20eb
zhouqiang <1020498110@qq.com>
2017-03-01 16:57:07 +0800

    添加百度云的ocr

--- app/src/main/java/com/enet/textbaiduapi/MainActivity.java
+++ app/src/main/java/com/enet/textbaiduapi/MainActivity.java
@@ -29,11 +29,15 @@ public class MainActivity extends AppCompatActivity {
 
     private JCameraView mJCameraView;
     private String s;
+    private String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        initIntent();
 
 
         View decorView = getWindow().getDecorView();
@@ -49,68 +53,25 @@ public class MainActivity extends AppCompatActivity {
         mJCameraView.setAutoFoucs(false);
         mJCameraView.setCameraViewListener(new JCameraView.CameraViewListener() {
 
-
-
             @Override
             public void quit() {
                 //返回按钮的点击时间监听
                 MainActivity.this.finish();
             }
+
             @Override
             public void captureSuccess(Bitmap bitmap) {
-              //  s = convertIconToString(bitmap);
+                s = convertIconToString(bitmap);
+                //  ocr();
 
+                //x新的文字识别
 
-                //s="/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDABMNDxEPDBMREBEWFRMXHTAfHRsbHTsqLSMwRj5KSUU+RENNV29eTVJpU0NEYYRiaXN3fX59S12Jkoh5kW96fXj/2wBDARUWFh0ZHTkfHzl4UERQeHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHj/wAARCAAfACEDAREAAhEBAxEB/8QAGAABAQEBAQAAAAAAAAAAAAAAAAQDBQb/xAAjEAACAgICAgEFAAAAAAAAAAABAgADBBESIRMxBSIyQXGB/8QAFAEBAAAAAAAAAAAAAAAAAAAAAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/APawEBAQEBAgy8i8ZTVV3UY6V1eU2XoWDDZB19S646Gz39w9fkKsW1r8Wm2yo1PYis1be0JG9H9QNYCAgc35Cl3yuVuJZl0cB41rZQa32dt2y6OuOiOxo61vsLcVblxaVyXD3hFFjL6La7I/sDWAgICAgICB/9k=";
-
-
-                OkHttpClient mOkHttpClient = new OkHttpClient();
-                RequestBody formBody = new FormBody.Builder()
-
-                        .add("fromdevice", "pc")
-                        .add("clientip", "10.10.10.0")
-                        .add("detecttype", "LocateRecognize")
-                        .add("languagetype", "CHN_ENG")
-                        .add("imagetype","3")
-                       // .add("image", "/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDABMNDxEPDBMREBEWFRMXHTAfHRsbHTsqLSMwRj5KSUU+RENNV29eTVJpU0NEYYRiaXN3fX59S12Jkoh5kW96fXj/2wBDARUWFh0ZHTkfHzl4UERQeHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHj/wAARCAAfACEDAREAAhEBAxEB/8QAGAABAQEBAQAAAAAAAAAAAAAAAAQDBQb/xAAjEAACAgICAgEFAAAAAAAAAAABAgADBBESIRMxBSIyQXGB/8QAFAEBAAAAAAAAAAAAAAAAAAAAAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/APawEBAQEBAgy8i8ZTVV3UY6V1eU2XoWDDZB19S646Gz39w9fkKsW1r8Wm2yo1PYis1be0JG9H9QNYCAgc35Cl3yuVuJZl0cB41rZQa32dt2y6OuOiOxo61vsLcVblxaVyXD3hFFjL6La7I/sDWAgICAgICB/9k=")
-                        .add("image", "http://pic1.nipic.com/2008-09-08/200898163242920_2.jpg")
-                       // .add("image", s)
-                        .build();
-                Request request = new Request.Builder()
-                        .url("http://apis.baidu.com/idl_baidu/baiduocrpay/idlocrpaid")
-                        .post(formBody)
-                        .addHeader("apikey", "974c5e58541dd1fa65d1785b2a7259d5")
-                        .build();
-                Call call = mOkHttpClient.newCall(request);
-                call.enqueue(new Callback() {
-                    @Override
-                    public void onFailure(Call call, IOException e) {
-
-                    }
-
-                    @Override
-                    public void onResponse(Call call, Response response) throws IOException {
-                        String str = response.body().string();
-                        Log.i("zq", str);
-
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(getApplicationContext(), "请求成功", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-
-                });
-
-
-
-
-
+                ocr2(s);
 
 
                 //获取到拍照成功后返回的Bitmap
             }
+
             @Override
             public void recordSuccess(String url) {
                 //获取成功录像后的视频路径
@@ -118,9 +79,121 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    private void ocr2(String s) {
 
 
+        OkHttpClient mOkHttpClient = new OkHttpClient();
+        RequestBody formBody = new FormBody.Builder()
 
+                .add("access_token", str)
+                .add("image", "/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDABMNDxEPDBMREBEWFRMXHTAfHRsbHTsqLSMwRj5KSUU+RENNV29eTVJpU0NEYYRiaXN3fX59S12Jkoh5kW96fXj/2wBDARUWFh0ZHTkfHzl4UERQeHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHj/wAARCAAfACEDAREAAhEBAxEB/8QAGAABAQEBAQAAAAAAAAAAAAAAAAQDBQb/xAAjEAACAgICAgEFAAAAAAAAAAABAgADBBESIRMxBSIyQXGB/8QAFAEBAAAAAAAAAAAAAAAAAAAAAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/APawEBAQEBAgy8i8ZTVV3UY6V1eU2XoWDDZB19S646Gz39w9fkKsW1r8Wm2yo1PYis1be0JG9H9QNYCAgc35Cl3yuVuJZl0cB41rZQa32dt2y6OuOiOxo61vsLcVblxaVyXD3hFFjL6La7I/sDWAgICAgICB/9k=")
+                .build();
+        Request request = new Request.Builder()
+                .url("https://aip.baidubce.com/rest/2.0/ocr/v1/general")
+                .post(formBody)
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .build();
+        Call call = mOkHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String str2 = response.body().string();
+                Log.i("ocrzq", str2);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "请求成功", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+        });
+
+
+    }
+
+    private void initIntent() {
+
+        OkHttpClient mOkHttpClient = new OkHttpClient();
+        RequestBody formBody = new FormBody.Builder()
+
+                .add("grant_type", "client_credentials")
+                .add("client_id", "QCdBRVGeGZu0BqtUneFjHbVY")
+                .add("client_secret", "5vKDF6oX2PDAPwmNZeXWaq6uABfk3T65")
+                .build();
+        Request request = new Request.Builder()
+                .url("https://aip.baidubce.com/oauth/2.0/token")
+                .post(formBody)
+                .build();
+        Call call = mOkHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e("zqNew", e.toString());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                str = response.body().string();
+                 Log.i("zqNew", str);
+            }
+        });
+
+
+    }
+
+    private void ocr() {
+        //s="/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDABMNDxEPDBMREBEWFRMXHTAfHRsbHTsqLSMwRj5KSUU+RENNV29eTVJpU0NEYYRiaXN3fX59S12Jkoh5kW96fXj/2wBDARUWFh0ZHTkfHzl4UERQeHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHj/wAARCAAfACEDAREAAhEBAxEB/8QAGAABAQEBAQAAAAAAAAAAAAAAAAQDBQb/xAAjEAACAgICAgEFAAAAAAAAAAABAgADBBESIRMxBSIyQXGB/8QAFAEBAAAAAAAAAAAAAAAAAAAAAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/APawEBAQEBAgy8i8ZTVV3UY6V1eU2XoWDDZB19S646Gz39w9fkKsW1r8Wm2yo1PYis1be0JG9H9QNYCAgc35Cl3yuVuJZl0cB41rZQa32dt2y6OuOiOxo61vsLcVblxaVyXD3hFFjL6La7I/sDWAgICAgICB/9k=";
+
+
+        OkHttpClient mOkHttpClient = new OkHttpClient();
+        RequestBody formBody = new FormBody.Builder()
+
+                .add("fromdevice", "pc")
+                .add("clientip", "10.10.10.0")
+                .add("detecttype", "LocateRecognize")
+                .add("languagetype", "CHN_ENG")
+                .add("imagetype", "3")
+                // .add("image", "/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDABMNDxEPDBMREBEWFRMXHTAfHRsbHTsqLSMwRj5KSUU+RENNV29eTVJpU0NEYYRiaXN3fX59S12Jkoh5kW96fXj/2wBDARUWFh0ZHTkfHzl4UERQeHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHj/wAARCAAfACEDAREAAhEBAxEB/8QAGAABAQEBAQAAAAAAAAAAAAAAAAQDBQb/xAAjEAACAgICAgEFAAAAAAAAAAABAgADBBESIRMxBSIyQXGB/8QAFAEBAAAAAAAAAAAAAAAAAAAAAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/APawEBAQEBAgy8i8ZTVV3UY6V1eU2XoWDDZB19S646Gz39w9fkKsW1r8Wm2yo1PYis1be0JG9H9QNYCAgc35Cl3yuVuJZl0cB41rZQa32dt2y6OuOiOxo61vsLcVblxaVyXD3hFFjL6La7I/sDWAgICAgICB/9k=")
+                .add("image", "http://pic1.nipic.com/2008-09-08/200898163242920_2.jpg")
+                // .add("image", s)
+                .build();
+        Request request = new Request.Builder()
+                .url("http://apis.baidu.com/idl_baidu/baiduocrpay/idlocrpaid")
+                .post(formBody)
+                .addHeader("apikey", "974c5e58541dd1fa65d1785b2a7259d5")
+                .build();
+        Call call = mOkHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String str = response.body().string();
+                Log.i("zq", str);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "请求成功", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+        });
     }
 
     @Override
@@ -128,19 +201,20 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         mJCameraView.onResume();
     }
+
     @Override
     protected void onPause() {
         super.onPause();
         mJCameraView.onPause();
     }
-/*    public static String convertIconToString(Bitmap bitmap)
-    {
+
+    public static String convertIconToString(Bitmap bitmap) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();// outputstream
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
         byte[] appicon = baos.toByteArray();// 转为byte数组
         return Base64.encodeToString(appicon, Base64.DEFAULT);
 
-    }*/
+    }
 
 
 }
